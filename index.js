c3aa504253913881c3120ad3954c1ec928cc795d
majaliju <majalijunyc@gmail.com>
2022-01-31 20:32:13 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+// a general writeup
+
+function findMatching(drivers, name){
+    let matchingList = drivers.filter(element => element === name || element === name.toLowerCase())
+    return matchingList
+}
+
+
+function fuzzyMatch(drivers, string){
+    let matchingLetters = drivers.filter(driver => driver.slice(0, string.length) === string)
+    return matchingLetters
+}
+
+
+function matchName(drivers, string){
+    let matchingNames =  drivers.filter(driver => driver["name"] === string)
+    return matchingNames
+}
